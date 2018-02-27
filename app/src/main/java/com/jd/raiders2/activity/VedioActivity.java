26afc140d4f76d27673a0ca6ca37fe456687f991
houhuang <houhuang@smalltreemedia.com>
2018-02-27 15:52:35 +0800

    no message

--- app/src/main/java/com/jd/raiders2/activity/VedioActivity.java
+++ app/src/main/java/com/jd/raiders2/activity/VedioActivity.java
@@ -173,9 +173,9 @@ public class VedioActivity extends AppCompatActivity {
             // 4. Change the state of the window
             VedioActivity.this.getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            view.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                             View.SYSTEM_UI_FLAG_FULLSCREEN |
                             View.SYSTEM_UI_FLAG_IMMERSIVE);
             VedioActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
